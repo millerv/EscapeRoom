4143e6e805fbd0bb293eb506856019f156867fc9
millerv <uwmillerv@gmail.com>
2016-11-13 21:28:55 -0800

    Initial commit for Udemy UE4 escape room project
    Preparing a .gitignore and the remaining files for the repo

---
+++ EscapeRoom/Source/EscapeRoom/EscapeRoomGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "EscapeRoomGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ESCAPEROOM_API AEscapeRoomGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
